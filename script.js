81f09fb617af4b3d4dbd3568f5842cd09282a24d
Thanh Nha Lam <lam00129@algonquinlive.com>
2021-04-13 18:09:29 -0400

    process

--- script.js
+++ script.js
@@ -26,6 +26,7 @@ const input = []
 input.push(`
     <h1>HELLO! IT'S CURRENTLY</h1>
     <div id="clock" class="clock"></div>
+    <div id="date" class="date"></div>
 `)
 
 $displayTime.innerHTML=input.join('')
@@ -36,68 +37,82 @@ setInterval(showTime, 1000);
 function showTime() {
     let time = new Date()
     let hour = time.getHours()
-    let min = time.getMinutes()
-    // let sec = time.getSeconds()
-    // am_pm = " AM";
-  
-    // if (hour > 12) {
-    //     hour -= 12
-    //     am_pm = " PM"
-    // }
-    // if (hour == 0) {
-    //     hr = 12
-    //     am_pm = "AM"
-    // }
-  
-    let currentTime = hour + ":" + min 
-            // + ":" + sec 
-            // + am_pm
+    let min = String(time.getMinutes()).padStart(2,"0")
+    let sec = String(time.getSeconds()).padStart(2,"0")
+    let currentTime = ''
+    let currentDate = ''
+
+    console.log(document.getElementById('seconds').checked)
+    if(document.getElementById('seconds').checked){
+        currentTime = hour + ":" + min + ":" + sec
+    }
+    else {
+        console.log("showTime")
+        currentTime = hour + ":" + min
+    }
   
     document.getElementById("clock").innerHTML = currentTime
+    console.log(new Date())
+    if(document.getElementById('yes-date').checked){
+        currentDate = new Date().toDateString()
+    }
+
+    document.getElementById("date").innerHTML = currentDate    
 }
 
-showTime()
 
 
 //SETTING BUTTON
 const input_a = []
 input_a.push(`
     <button id="setting-button">SETTING</button>
+    <div id="create-form"></div>
 `)
 $setting.innerHTML=input_a.join('')
 
+const $createForm = document.getElementById('create-form')
+const input_setting_btn = []
+input_setting_btn.push(`
+    <form id="form-setting">
+        <h1>SETTINGS</h1>
+        <div id="setting-section">
+            <div id="show-seconds">
+                <h2>SHOW SECONDS</h2>
+                <input type="radio" name="answer-seconds" id="seconds">
+                <label for="yes">Yes</label><br>
+                <input type="radio" name="answer-seconds" checked>
+                <label for="no">No</label><br>
+            </div>
+            <div id="show-date">
+                <h2>SHOW DATE</h2>
+                <input type="radio" name="answer-date" id="yes-date">
+                <label for="yes">Yes</label><br>
+                <input type="radio" name="answer-date" checked>
+                <label for="no">No</label><br>
+            </div>
+        </div>
+        <button id="save-setting">CLOSE</button>
+    </form>
+`)
+$createForm.innerHTML=input_setting_btn.join('')
+$createForm.classList.add('hide')
+
+showTime()
+
 const $settingButton = document.getElementById('setting-button')
 $settingButton.addEventListener('click', function(e){
-    const input_setting_btn = []
-    input_setting_btn.push(`
-        <form id="form-setting">
-            <h1>SETTINGS</h1>
-            <div id="24-hour">24-HOUR CLOCK<br>
-                <input type="radio">
-                <label for="yes">Yes</label><br>
-                <input type="radio">
-                <label for="no">No</label><br>
-            </div>
-            <div id="show-seconds">SHOW SECONDS<br>
-                <input type="radio">
-                <label for="yes">Yes</label><br>
-                <input type="radio">
-                <label for="no">No</label><br>
-            </div>
-            <button id="save-setting">SAVE</button>
-        </form>
-    `)
-    $settingButton.innerHTML=input_setting_btn.join('')
+
+    $settingButton.classList.add('hide')
+    $createForm.classList.remove('hide')
 
     const $settingForm = document.getElementById('form-setting')
-
     const $saveSettingButton = document.getElementById('save-setting')
-    const $hideSetting = document.getElementById('hide-setting')
+    // const $hideSetting = document.getElementById('hide-setting')
     $saveSettingButton.addEventListener('click', function(a){
         a.preventDefault()
-
-        $settingForm.classList.add('hide-setting')
-
+        showTime()
+        $createForm.classList.add('hide')
+        $settingButton.classList.remove('hide')
     })
 })
 
@@ -107,28 +122,98 @@ $settingButton.addEventListener('click', function(e){
 const input_b = []
 input_b.push(`
     <button id="more-button">MORE</button>
+    <div id="more-options" class="hide"></div>
 `)
 $more.innerHTML=input_b.join('')
 
 const $moreButton = document.getElementById('more-button')
+const $moreOptions = document.getElementById('more-options')
+
 $moreButton.addEventListener('click', function(b){
     const input_more_btn = []
     input_more_btn.push(`
-        <div id="more-options">
-            <div id="day-of-week">DAY OF THE WEEK<br>
+        <div id="more-section">
+            <div>
+                <h1>DAY OF THE WEEK</h1>
+                <div id="day-of-week"></div>
             </div>
-            <div id="day-of-month">DAY OF THE MONTH<br>
+            <div>
+                <h1>DAY OF THE MONTH</h1>
+                <div id="day-of-month"></div>
             </div>
-            <div id="day-of-year">DAY OF THE YEAR<br>
+            <div>
+                <h1>DAY OF THE YEAR</h1>
+                <div id="day-of-year"></div>
+            </div>
+            <div>
+                <h1>WEEK OF THE YEAR</h1>
+                <div id="week-of-year"></div>
             </div>
         </div>
+        <button id="close">CLOSE</button>
     `)
-    $moreButton.innerHTML=input_more_btn.join('')
 
-    let $dayOfWeek = document.getElementById('day-of-week')
-    let $dayOfMonth = document.getElementById('day-of-month')
-    let $dayOfYear = document.getElementById('day-of-year')
+    $moreOptions.innerHTML=input_more_btn.join('')
+    
+    
+    $moreButton.classList.add('hide')
+    $moreOptions.classList.remove('hide')
 
+    const $dayOfWeek = document.getElementById('day-of-week')
+    const $dayOfMonth = document.getElementById('day-of-month')
+    const $dayOfYear = document.getElementById('day-of-year')
+    const $weekOfYear = document.getElementById('week-of-year')
+
+    //DAY OF WEEK
+    function showDay() {
+        let d = new Date()
+        let weekday = new Array(7)
+        weekday[0] = "Sunday"
+        weekday[1] = "Monday"
+        weekday[2] = "Tuesday"
+        weekday[3] = "Wednesday"
+        weekday[4] = "Thursday"
+        weekday[5] = "Friday"
+        weekday[6] = "Saturday"
+        let day = weekday[d.getDay()]
+        let currentDay = day
+        document.getElementById("day-of-week").innerHTML = currentDay
+    }
+    showDay()
+
+    //DAY OF MONTH
+    function showMonth() {
+        let d = new Date()
+        let month = d.getDate()
+        let currentMonth = month
+        document.getElementById("day-of-month").innerHTML = currentMonth
+    }
+    showMonth()
+
+    //DAY OF YEAR
+    function showYear() {
+        const dayOfYear = date => 
+        Math.floor((date - new Date(date.getFullYear(), 0, 0)) / (1000 * 60 * 60 * 24));
+        let currentYear = dayOfYear(new Date())
+        document.getElementById("day-of-year").innerHTML = currentYear
+    }
+    showYear()
+
+    //WEEK OF YEAR
+    function showWeek(){
+        let d = new Date()
+        let x =  new Date(d.getFullYear(), 0, 1)
+        let numberOfDays =  Math.floor((d - x) / (24 * 60 * 60 * 1000))
+        let result = Math.ceil((d.getDay() + 1 + numberOfDays) / 7) 
+        document.getElementById("week-of-year").innerHTML = result
+    }
+    showWeek()
+
+    //CLOSE BUTTON
+    const $close = document.getElementById('close')
+    $close.addEventListener('click',function(c){
+        $moreButton.classList.remove('hide')
+        $moreOptions.classList.add('hide')
+    })
 
 })
-console.log(showTime)
